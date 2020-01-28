1d23397d7e0d04ae5644a821cc7766406b72cdf3
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2020-01-27 19:54:08 -0500

    Update Hex-Bin.C

--- Hex-Bin.C
+++ Hex-Bin.C
@@ -24,8 +24,49 @@ int hex_binary(char hex[], int binary_number[])
 }
 
 
-void(long long binary, char[]){
+void(long long binary,char hex[]){
 
-  
+   int hexConstant[] = {0, 1, 10, 11, 100, 101, 110, 111, 1000, 
+                         1001, 1010, 1011, 1100, 1101, 1110, 1111};
+
+    long long tempBinary;
+    int index, i, digit;
+     tempBinary = binary;
+    index = 0;
+    
+    /* Find hexadecimal of binary number */
+    while(tempBinary!=0)
+    {
+        /* Group binary to last four digits */
+        digit = tempBinary % 10000;
+
+        /* Find hexadecimal equivalent of last four digit */
+        for(i=0; i<16; i++)
+        {
+            if(hexConstant[i] == digit)
+            {
+                if(i<10)
+                {
+                    /* 0-9 integer constant */
+                    hex[index] = (char)(i + 48);
+                }
+                else
+                {
+                    /* A-F character constant */
+                    hex[index] = (char)((i-10) + 65);
+                }
+
+                index++;
+                break;
+            }
+        }
+
+        /* Remove the last 4 digits as it is processed */
+        tempBinary /= 10000;
+    }
+    hex[index] = '\0';
+
+    /* Reverse the hex digits */
+    strrev(hex);
 
 }
